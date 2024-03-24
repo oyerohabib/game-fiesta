509ba464a8328a18c57f6bb06028234dd564bfe7
oyerohabib <oyerohabib@yahoo.com>
2024-03-24 23:33:37 +0100

    chore: removed unused dependencies to fix build error

--- src/components/DefinitionItem.tsx
+++ src/components/DefinitionItem.tsx
@@ -1,22 +1,20 @@
-import { Box, Heading } from '@chakra-ui/react';
-import React, { ReactNode } from 'react'
+import { Box, Heading } from "@chakra-ui/react";
+import { ReactNode } from "react";
 
 interface Props {
-  term: string;
-  children: ReactNode | ReactNode[];
+    term: string;
+    children: ReactNode | ReactNode[];
 }
 
 const DefinitionItem = ({ term, children }: Props) => {
-  return (
-    <Box marginY={5}>
-      <Heading as='dt' fontSize='md' color='gray.600'>
-        {term}
-      </Heading>
-      <dd>
-        {children}
-      </dd>
-    </Box>
-  )
-}
+    return (
+        <Box marginY={5}>
+            <Heading as="dt" fontSize="md" color="gray.600">
+                {term}
+            </Heading>
+            <dd>{children}</dd>
+        </Box>
+    );
+};
 
-export default DefinitionItem
+export default DefinitionItem;
